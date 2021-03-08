86f4df3384ef9f8931fd0ddf9e554c07641548ae
Euiman KIm <74029470+euimankim@users.noreply.github.com>
2021-03-08 17:56:25 +0900

    Update import_data_hana_cloud.sql

--- import_data_hana_cloud.sql
+++ import_data_hana_cloud.sql
@@ -1,4 +1,6 @@
 --  DEVUSER로 실행
+
+-- Step 1
 import from csv file
 's3-us-east-2://AKIAIKRDGYHC2YTW64SA:M7MjJUTtOYA11PsSbEVWsBoHzGAWPLgPwTTEhJE2@hanacloud/customer.dat'
 into CUSTOMER
@@ -6,6 +8,10 @@ with
 field delimited by '|'
 threads 4;
 
+-- Step 2
+select count(*) from CUSTOMER;
+
+-- Step 3
 import from csv file
 's3-us-east-2://AKIAIKRDGYHC2YTW64SA:M7MjJUTtOYA11PsSbEVWsBoHzGAWPLgPwTTEhJE2@hanacloud/web_sales.dat'
 into WEB_SALES
@@ -13,6 +19,5 @@ with
 field delimited by '|'
 threads 4;
 
-select count(*) from CUSTOMER;
-
+-- Step 4
 select count(*) from WEB_SALES;
