eef425bdfa84ab27a75fbccd108d55864d664306
Euiman KIm <74029470+euimankim@users.noreply.github.com>
2021-03-08 17:58:00 +0900

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,10 @@
+-- Step 1
 select c_customer_id, c_first_name || ' ' || c_last_name, sum(ws_net_profit)
 from customer, web_sales
 where c_customer_sk = ws_bill_customer_sk
 group by c_customer_id, c_first_name || ' ' || c_last_name;
 
+-- Step 2
 select c_customer_id, c_first_name || ' ' || c_last_name, sum(ss_net_profit)
 from customer, dl_store_sales
 where c_customer_sk = ss_customer_sk
